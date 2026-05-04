2b5afd17b2e2ee1f3f53676643beae2d1802a7dd
Rohan Hudati <rohanabcd0305@gmail.com>
2026-05-05 00:15:19 +0530

    feat: add HowItWorks and UseCases components to showcase functionality

--- app/page.tsx
+++ app/page.tsx
@@ -4,18 +4,28 @@ import Footer from "@/components/main/footer";
 import Showcase from "@/components/main/Showcase";
 import LogoStrip from "@/components/main/LogoStrip";
 import SplitFeature from "@/components/main/SplitFeature";
-
+import HowItWorks from "@/components/main/HowItWorks";
+import UseCases from "@/components/main/UseCases";
 
 export default function Page() {
   return (
     <main>
       <Navbar />
+
 <Hero />
+
 <LogoStrip />
+
 <SplitFeature />
 
 <Showcase />
+
+<HowItWorks />
+
+<UseCases /> 
+
 <Footer />
+
     </main>
   );
 }
